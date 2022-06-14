637fcbb3ad798b1edb1d408537307e25c311599d
min33sky <min33sky@naver.com>
2022-06-14 23:37:06 +0900

    :lipstick: comment ui

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { GET_ALL_POSTS, GET_ALL_POSTS_BY_TOPIC } from '../graphql/query';
 import { Post as PostType } from '../types/reddit';
 import Post from './Post';
+import { Jelly } from '@uiball/loaders';
 
 function Feed({ topic }: { topic?: string }) {
   const { data, error, loading } = useQuery(topic ? GET_ALL_POSTS_BY_TOPIC : GET_ALL_POSTS, {
@@ -15,8 +16,8 @@ function Feed({ topic }: { topic?: string }) {
 
   if (loading)
     return (
-      <div>
-        <p>Loading....</p>
+      <div className="flex w-full items-center justify-center p-10 text-left">
+        <Jelly size={50} color="#FF4501" />
       </div>
     );
 
--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import React from 'react';
 import Post from '../../components/Post';
@@ -9,6 +10,8 @@ function PostDetail() {
   const {
     query: { postId },
   } = useRouter();
+
+  const { data: session } = useSession();
 
   const { data } = useQuery(GET_POST_BY_POST_ID, {
     variables: {
@@ -21,6 +24,29 @@ function PostDetail() {
   return (
     <div className="mx-auto my-7 max-w-5xl">
       <Post post={post} />
+
+      {/* 댓글창 */}
+      <div className="-mt-1 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16">
+        <p className="text-sm">
+          Comment as <span className="text-red-500">{session?.user?.name}</span>
+        </p>
+
+        <form className="flex flex-col space-y-2">
+          <textarea
+            disabled={!session}
+            className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:cursor-not-allowed disabled:bg-gray-50"
+            placeholder={session ? 'What are you thoughts?' : 'Please Sign In to comment'}
+          />
+
+          <button
+            type="submit"
+            disabled={!session}
+            className="rounded-full bg-red-500 p-3 font-semibold text-white transition hover:bg-red-600 disabled:cursor-not-allowed disabled:bg-gray-400"
+          >
+            Comment
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
